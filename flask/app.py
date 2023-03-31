1784a5d702f1e983673aa0733ab82ba4893b6eee
Nischhal Neupane <np03cs4s210214@heraldcollege.edu.np>
2023-03-31 21:50:53 +0545

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,23 @@
 from flask import Flask
 
+
+nltk.download('punkt')
+nltk.download('stopwords')
+db = SQLAlchemy()
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app = Flask(__name__)
+bcrypt = Bcrypt(app)
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+db.init_app(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
 app = Flask(name)
 
 @app.route('/hello')
